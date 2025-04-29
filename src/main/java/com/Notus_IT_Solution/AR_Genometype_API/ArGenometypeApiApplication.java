bbd07b710059808ca77991ecdfdf571355c62527
Patchara <patchara.pon@no-tus.com>
2025-04-29 15:31:25 +0700

    fix-springboot-setup#2
    -add dependencies
    1. Spring Web
    2. Spring boot dev tools
    3. Spring data JPA
    4. Oracle Driver

---
+++ src/main/java/com/Notus_IT_Solution/AR_Genometype_API/ArGenometypeApiApplication.java
@@ -0,0 +1,13 @@
+package com.Notus_IT_Solution.AR_Genometype_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArGenometypeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArGenometypeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Notus_IT_Solution/AR_Genometype_API/ArGenometypeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Notus_IT_Solution.AR_Genometype_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArGenometypeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
